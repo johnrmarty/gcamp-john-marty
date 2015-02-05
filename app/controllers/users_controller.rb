28c7dffa23aca918a3bc171148b64eb23c2a5b7f
John Marty <JohnRmarty@gmail.com>
2015-02-04 17:53:12 -0700

    added users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,15 @@ class UsersController < ApplicationController
       end
     end
 
+
     def update
-      @user = User.find(params[:id])
-      @user.update_attributes(user_params)
-      redirect_to events_path
+     @user = User.find(params[:id])
+     if @user.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated'
+    else
+      render :edit
     end
+  end
 
 
 
@@ -29,9 +33,13 @@ class UsersController < ApplicationController
     end
 
     def show
+      @user = User.find(params[:id])
     end
 
     def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to users_path, notice: 'User was successfully destroyed.'
     end
 
     private
