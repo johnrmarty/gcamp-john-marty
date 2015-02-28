3e903b7046d4d49cca3209e12c6f360a17fbc6c6
John Marty <JohnRmarty@gmail.com>
2015-02-28 14:05:11 -0700

    add capybara coverage to signin for valid and invalid cases

---
+++ spec/features/signin_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+
+describe 'User can sign-in' do 
+	
+
+	before :each do 
+
+	 User.create(email: 'q@q.com', password: 'q', first_name: 'john', last_name: 'marty')	
+	
+	
+	end 
+
+
+	 scenario 'Users can sign-in with valid information' do
+
+	 	visit '/'
+		click_on "Signin"
+		fill_in 'Email', :with => "q@q.com"
+		fill_in 'Password', :with => "q"
+		click_on "Sign in"		
+
+		expect(page).to have_content("You are signed in") 
+
+	end 
+
+	scenario 'Users can NOT sign-in with invalid information' do
+
+	 	visit '/'
+		click_on "Signin"
+		fill_in 'Email', :with => ""
+		fill_in 'Password', :with => ""
+		click_on "Sign in"		
+
+		expect(page).to have_content("Invalid email or password") 
+
+
+
+
+end 
+end 
