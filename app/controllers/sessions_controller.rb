c13fcca0c04585eb732091107822f24dfdd334f5
John Marty <JohnRmarty@gmail.com>
2015-04-11 12:13:04 -0600

    When an existing user logs in, they are  redirected to the projects index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			flash[:notice] = "You are signed in"
-			redirect_to root_path
+			redirect_to projects_path
 		else
 			flash.now[:notice] = "Invalid email or password"
 			render 'new'
